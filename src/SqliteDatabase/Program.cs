ab3a0c08c95cb612c3a64e526896924916e819c2
Charles Farris <charlesfarris71@gmail.com>
2023-02-20 14:07:36 -0700

    Add project

---
+++ src/SqliteDatabase/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Autofac;
+using SleepingBearSystems.Tools.Persistence.Sqlite;
+
+namespace SleepingBearSystems.ToolsSamples.SqliteDatabase;
+
+internal static class Program
+{
+    public static int Main(string[] args)
+    {
+        try
+        {
+            var path = Path.Combine(Path.GetTempPath(), "sbs_sqlite.db");
+            using var container = new ContainerBuilder()
+                .RegisterSqliteDatabase("sbs_sqlite")
+                .Build();
+
+            using var scope = container.BeginLifetimeScope();
+
+            return 0;
+        }
+        catch (Exception e)
+        {
+            return 1;
+        }
+    }
+}
